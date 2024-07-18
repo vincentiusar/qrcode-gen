9fecdeea6dba2c2f3c4bffcc80c8a59b0a5ecd34
Vincentius Arnold <vincentiusarnoldfridolin@gmail.com>
2024-07-18 21:48:06 +0700

    Add more qrcode

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ app.use(cors());
 
 app.get('/', async (req, res) => {
     console.log(await qr.create("HAI"));
-    const result = await qr.toDataURL("https://docs.google.com/forms/d/e/1FAIpQLScEXcYgDu01W8GodEO2cHdeo8JppUwo8FTeJCGT1CcZo08DxQ/viewform?usp=pp_url&entry.1579164508=b");
+    const result1 = await qr.toDataURL("https://docs.google.com/forms/d/e/1FAIpQLScEXcYgDu01W8GodEO2cHdeo8JppUwo8FTeJCGT1CcZo08DxQ/viewform?usp=pp_url&entry.1579164508=b");
+    const result2 = await qr.toDataURL("https://docs.google.com/forms/d/e/1FAIpQLScEXcYgDu01W8GodEO2cHdeo8JppUwo8FTeJCGT1CcZo08DxQ/viewform?usp=pp_url&entry.1579164508=a");
 
-    return res.send(`<img src="${result}" alt="test"/><br/><p style="white-space:wrap;word-break: break-all;">${result}</p>`);
+    return res.send(`<img src="${result1}" alt="test"/><br/><p style="white-space:wrap;word-break: break-all;">${result1}</p><img src="${result2}" alt="test"/><br/><p style="white-space:wrap;word-break: break-all;">${result2}</p>`);
 });
 
 app.listen(3000, () => console.log("LISTENING 3000"));
