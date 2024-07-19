01fbf6f81e510f05d24a465003aa646316d5bcf0
Vincentius Arnold <vincentiusarnoldfridolin@gmail.com>
2024-07-19 21:22:06 +0700

    Add security code

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express');
 const cors = require('cors');
 const session = require('express-session');
 const qr = require('qrcode');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const path = require('path');
+
 const app = express();
 
 app.use(express.json());
@@ -20,12 +24,83 @@ app.use(cors());
 
 const { users } = require('./models');
 const { urls } = require('./models');
+// https://docs.google.com/forms/d/e/1FAIpQLScEXcYgDu01W8GodEO2cHdeo8JppUwo8FTeJCGT1CcZo08DxQ/viewform?usp=pp_url&entry.1579164508=b
+const encodedUrl = "https://rajawalichurch.my.id/tap?key=";
+const baseUrl = "https://docs.google.com/forms/d/e/1FAIpQLScEXcYgDu01W8GodEO2cHdeo8JppUwo8FTeJCGT1CcZo08DxQ/formResponse?usp=pp_url&entry.1579164508=";
+
+const loginCheck = async (req, res, next) => {
+    if (!req.session.token) {
+        return res.send("Unauthenticated");
+    }
+    
+    try {
+        await jwt.verify(req.session.token, process.env.SESSION_SECRET);
+        return next();
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+app.get('/gen', async (req, res) => {
+    const data = req.query?.name;
+    if (!data) {
+        return res.send("DATA NOT VALID");
+    }
+    const form = `${encodedUrl}${encodeURI(data)}`;
+    const result1 = await qr.toDataURL(form);
+    await urls.findOrCreate({
+        where: { key: data },
+        defaults: {
+            key: data,
+            value: form,
+            createdAt: Date.now(),
+            updatedAt: Date.now()
+        }
+    });
+
+    return res.send(`<img src="${result1}" alt="test"/>`);
+});
 
 app.get('/', async (req, res) => {
-    const result1 = await qr.toDataURL("https://docs.google.com/forms/d/e/1FAIpQLScEXcYgDu01W8GodEO2cHdeo8JppUwo8FTeJCGT1CcZo08DxQ/viewform?usp=pp_url&entry.1579164508=b");
-    const result2 = await qr.toDataURL("https://docs.google.com/forms/d/e/1FAIpQLScEXcYgDu01W8GodEO2cHdeo8JppUwo8FTeJCGT1CcZo08DxQ/viewform?usp=pp_url&entry.1579164508=a");
+    if (req.session.token) {
+        return res.send('Welcome :D');
+    }
+    return res.send('welcome?');
+});
 
-    return res.send(`<img src="${result1}" alt="test"/><br/><p style="white-space:wrap;word-break: break-all;">${result1}</p><img src="${result2}" alt="test"/><br/><p style="white-space:wrap;word-break: break-all;">${result2}</p>`);
+app.get('/tap', loginCheck, async (req, res) => {
+    if (req.query?.key) {
+        const data = await urls.findOne({
+            where: { key: req.query.key }
+        });
+
+        if (data) {
+            try {
+                return res.redirect(data.value);
+            } catch (e) {
+                return res.redirect(baseUrl+key);
+            }
+        }
+    }
+
+    return res.send("Unauthenticated");
+});
+
+app.get('/login', async (req, res) => {
+    res.sendFile(path.join(__dirname, '/login.html'));
+});
+
+app.post('/login', async (req, res) => {
+    if (req.body && req?.body?.username) {
+        const user = await users.findOne({ where: {username: req.body.username }});
+        if (user?.username == req.body.username && bcrypt.compare(user?.password, req.body.password)) {
+            const token = jwt.sign(req.body, process.env.SESSION_SECRET, { expiresIn: '1d' });
+
+            let session = req.session;
+            session.token = token;
+        }
+    }
+    return res.redirect('/');
 });
 
 app.listen(3000, () => console.log("LISTENING 3000"));
