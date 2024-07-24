f170ce6905510d6adc5a9b0c43c207a04eb683a6
Vincentius Arnold <vincentiusarnoldfridolin@gmail.com>
2024-07-24 22:29:36 +0700

    Again

--- app.js
+++ app.js
@@ -120,11 +120,11 @@ app.get('/tap', loginCheck, async (req, res) => {
             try {
                 return res.redirect(data.value);
             } catch (e) {
-                const url = `https://docs.google.com/forms/d/e/1FAIpQLScEXcYgDu01W8GodEO2cHdeo8JppUwo8FTeJCGT1CcZo08DxQ/formResponse?usp=pp_url&entry.1579164508=${encodeURI(data.key)}&entry.41140801=${backup.ref}&entry.615117972=${backup.email}`;
+                const url = `https://docs.google.com/forms/d/e/1FAIpQLScEXcYgDu01W8GodEO2cHdeo8JppUwo8FTeJCGT1CcZo08DxQ/formResponse?usp=pp_url&entry.1579164508=${encodeURI(backup.key)}&entry.41140801=${backup.ref}&entry.615117972=${backup.email}`;
                 return res.redirect(url);
             }
         } else {
-            const url = `https://docs.google.com/forms/d/e/1FAIpQLScEXcYgDu01W8GodEO2cHdeo8JppUwo8FTeJCGT1CcZo08DxQ/formResponse?usp=pp_url&entry.1579164508=${encodeURI(data.key)}&entry.41140801=${backup.ref}&entry.615117972=${backup.email}`;
+            const url = `https://docs.google.com/forms/d/e/1FAIpQLScEXcYgDu01W8GodEO2cHdeo8JppUwo8FTeJCGT1CcZo08DxQ/formResponse?usp=pp_url&entry.1579164508=${encodeURI(backup.key)}&entry.41140801=${backup.ref}&entry.615117972=${backup.email}`;
             return res.redirect(url);
         }
     }
